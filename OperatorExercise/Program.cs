026c46fbfd4c268b57eb73f3bfaf89db7af5b25a
Raynard Brown <brown_raynard@icloud.com>
2024-08-12 13:40:04 -0500

    OperatorsExercise1

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // two integer variables
+            int a = 17;
+            int b = 4;
+
+
+            // Operator:
+            // Additcion: x + y
+            // Subraction: x - y
+            // Multiply: x * y
+            // Division x / y
+            // Modulas: x % y
+
+            int sum = a + b;
+            int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine(" 17/4 is 4 remainder 1");
+            
+
         }
+
+        
+        
+            
+        
+        
     }
+
 }
