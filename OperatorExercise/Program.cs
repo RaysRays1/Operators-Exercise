c61dec1bb2a6c697a7801734c73dc7e3eec5513b
Raynard Brown <brown_raynard@icloud.com>
2024-08-12 21:41:14 -0500

    operators-EX 1&2

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -19,15 +19,42 @@
             int sum = a + b;
             int quotient = a / b;
             int remainder = a % b;
-            Console.WriteLine(" 17/4 is 4 remainder 1");
+            // display results with string interpolation
+            Console.WriteLine($"{a}/{b} is {quotient} remainder{1}");
             
+            //prompt
+            Console.WriteLine("Enter in a radius");
+            
+            //user input as a string
+            string userInput = Console.ReadLine();
+            
+            //parse string into an double
+            double parsedRadius = double.Parse(userInput);
+            
+            // Asking for radius of circle
+            // Double radius = AreaofCircle
+            // Console.WriteLine(answer);
+            
+            Console.WriteLine("Please input the radius value for a circle");
+            double radius = double.Parse(Console.ReadLine());
+            double answer = AreaofCircle(radius);
+            Console.WriteLine($"The value of the area of the circle is {answer}");
+            
+        }
+        //Method for AreaofCircle
+
+        public static double AreaofCircle(double radius)
+        {
+            // Math.PI * Math.Pow(radius, 2);
+            return (Math.PI * Math.Pow(radius, 2));
 
         }
 
-        
-        
-            
-        
+        static int Add(int number1, int number2)
+            {
+                int sum = number1 + number2;
+                return sum;
+            }
         
     }
 
